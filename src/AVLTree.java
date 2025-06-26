55828d46fb58b282901d4c2015b1fa4e0f3bc27e
jonnathans06 <jhonasaav@gmail.com>
2025-06-26 08:51:13 -0500

    Cambios

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -1,40 +1,91 @@
 public class AVLTree {
-    private Node Node;
 
-    public AVLTree (){
-        this.Node = null;
+    private Node root;
+
+    public AVLTree(){
+        this.root = null;
     }
+    public int height(Node node){//altura de el NODO
+        if(node== null){//para sacar la altura se pregunta a los hijos que altura teienen, se saca el maximo entre left&right y se suma 1
+            return 0;
 
-    public int height(Node node) {
-        if (node == null) {
+        }
+        return node.getHeight();
+    }
+    public int getBalance(Node node){
+        if(node==null){
             return 0;
         }
-        return node.getHeigth();
-    }
+        return height(node.getLeft())-height(node.getRight());
 
-    public int getBalance(){
-        return 0;
     }
-
     public void insert(int value){
-        root = insertRec(Node, value);
+
+        System.out.println("Nodo a insertar: ");
+        root=insertRec(root, value);
     }
 
-    private Node insertRec (Node node, int value){
-        if (node == null) {
-            Node newNode = new Node(value);
-            newNode.setHeigth(1);
+    private Node insertRec(Node node,int value){
+        if(node==null){
+            Node newNode= new Node(value);
+            newNode.setHeight(1);
+            System.out.println("Nodo insertado: "+newNode.getValue()+"Balance a insertar: " + getBalance(newNode));
             return newNode;
-        } 
-        if (value < node.getValue()) {
-            node.setLeft(insertRec(node, value));
-        } else if (value > node.getValue()) {
+        }
+
+        if(value<node.getValue()){
+            node.setLeft(insertRec(node.getLeft(), value));
+
+        }else if(value>node.getHeight()){
             node.setRight(insertRec(node.getRight(), value));
-        } else {
+            
+        }else {
             return node;
         }
 
+        System.out.println("Node actual: "+ node.getValue());
+
+        //actualizar la altura de este anceestro Node
+        int altura= 1+ Math.max(height(node.getLeft()), height(node.getRight()));
+        node.setHeight(altura);
+        System.out.println("\tAltura = "+ node.getHeight());
+
+        int balance = getBalance(node);
+        System.out.println("\tBalance = "+ balance);
+
+
+        //caso izquierda-izquierda diagonal izq
+        if(balance>1&& value<node.getLeft().getValue()){
+            System.out.println("rotacion derecha: ");
+
+        }
+        //caso derecha-derecha diagonal derecha
+        if(balance<-1 && value>node.getRight().getValue()){
+            //si el balance es menor a -1 el nodo con mayor altura es 
+            System.out.println("rotacion izquierda: ");
+        }
+        //caso izq-derecha
+        if(balance>1 && value>node.getLeft().getValue()){
+            //si el balance es mayor a 1 el que tiene mayor altura es el de la derecha (porque formula es izq-dere)
+            System.out.println("rotacion izquierda derecha: ");
+        }
+
+        //caso derecha-izquierda
+        if(balance<-1 && value<node.getRight().getValue()){
+            //si el balance es menor a -1 el nodo con mayor altura es 
+
+            System.out.println("rotacion derecha izquierda: ");
+        }
         
+        
+        
+        
+        
+        
+        
+        return node;
+
+
     }
 
 }
--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        System.out.println("Nombre: Jonnathan Saavedra");
+
+        AVLTree tree = new AVLTree();
+
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(15);
+        
+        System.out.println(tree);
     }
 }
